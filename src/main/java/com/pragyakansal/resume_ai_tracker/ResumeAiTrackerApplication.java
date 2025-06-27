98cb7477d09ca0856cc3df3d79c0417fceb9be60
Pragya Kansal <pragyakansal419@gmail.com>
2025-06-27 15:22:42 -0500

    Initial Resume AI Tracker project setup

---
+++ src/main/java/com/pragyakansal/resume_ai_tracker/ResumeAiTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pragyakansal.resume_ai_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAiTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAiTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragyakansal/resume_ai_tracker/ResumeAiTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragyakansal.resume_ai_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAiTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
